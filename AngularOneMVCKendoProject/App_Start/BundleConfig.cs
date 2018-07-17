de68ae392d2de087bfa6aeba6b2cd725b908dc47
emadsaber <omda.sharkawy@gmail.com>
2018-07-17 23:54:12 +0200

    Adding KendoUI For JQuery

--- AngularOneMVCKendoProject/App_Start/BundleConfig.cs
+++ AngularOneMVCKendoProject/App_Start/BundleConfig.cs
@@ -9,37 +9,41 @@ namespace AngularOneMVCKendoProject
         public static void RegisterBundles(BundleCollection bundles)
         {
             #region Common
-            
+
             #region Scripts
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                                "~/Assets/Scripts/Common/jquery.min.js"));
+                "~/Assets/Scripts/Common/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                            "~/Assets/Scripts/Common/angular.min.js"));
+                "~/Assets/Scripts/Common/angular.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Assets/Scripts/Common/bootstrap.js",
-                      "~/Assets/Scripts/Common/respond.js"));
+                "~/Assets/Scripts/Common/bootstrap.min.js",
+                "~/Assets/Scripts/Common/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
-                            "~/Assets/Scripts/Common/kendo.aspnetmvc.min.js",
-                            "~/Assets/Scripts/Common/kendo.aspnetmvc.min.js"));
+                "~/Assets/Scripts/Common/kendo.all.min.js"));
             #endregion
 
             #region Styles
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                  "~/Assets/Styles/Common/bootstrap.css",
-                  "~/Assets/Styles/Main/MainStyle.css"));
+                "~/Assets/Styles/Main/MainStyle.css"));
 
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
-                  "~/Assets/Styles/Common/bootstrap.css"));
+                "~/Assets/Styles/Common/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendoThemes/Fiori").Include(
+                "~/Assets/Styles/Common/kendo.fiori.min.css"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/kendoThemes/Black").Include(
+                "~/Assets/Styles/Common/kendo.black.min.css"
+                ));
 
             bundles.Add(new StyleBundle("~/Content/kendo").Include(
-                  "~/Assets/Styles/Common/kendo.common.min.css",
-                  "~/Assets/Styles/Common/kendo.default.min.css",
-                  "~/Assets/Styles/Common/kendo.fiori.min.css",
-                  "~/Assets/Styles/Common/kendo.rtl.min.css"
+                "~/Assets/Styles/Common/kendo.common.min.css",
+                "~/Assets/Styles/Common/kendo.rtl.min.css"
                   ));
             #endregion
 
@@ -50,9 +54,9 @@ namespace AngularOneMVCKendoProject
             #region Main
 
             bundles.Add(new ScriptBundle("~/bundles/Modules/Main").Include(
-                    "~/Assets/Scripts/Modules/Main/Controllers/MainController.js",
-                    "~/Assets/Scripts/Modules/Main/Services/MainService.js",
-                    "~/Assets/Scripts/Modules/Main/MainApp.js")); 
+                "~/Assets/Scripts/Modules/Main/Controllers/MainController.js",
+                "~/Assets/Scripts/Modules/Main/Services/MainService.js",
+                "~/Assets/Scripts/Modules/Main/MainApp.js"));
 
             #endregion
 
